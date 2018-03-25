4bb6e0e857b3e90b9543db0f84bdc635b9851322
ajung <cwhtha@gmail.com>
2018-03-25 18:41:30 +0900

    0325 OAuth

---
+++ app/src/main/java/TwitterConsumer.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Hyunah on 2018-03-25.
+ * this is my access token
+ */
+
+public class TwitterConsumer extends Thread{
+    private final String ACCESS_TOKEN = "1208262667-ZipmRkLYxNPJUAZuq5RdFAvZ5abQoBBKpafOzCd";
+    private final String ACCESS_SECRET = "aFTq3FeiplifJdo5mUtNr0AZDJX9NbieCGYqPrTKEgUEo";
+    private final String CONSUMER_KEY = "9PIn71z3n1UH8mC50LKYSVA8c";
+    private final String CONSUMER_SECRET = "eVtDENEpzalS7jW7VVGWXOZBTZBOVlh6wYIHTHQoVIeuispbUH";
+
+    private final String FEED_URL = "https://stream.twitter.com/1.1/statuses/filter.json?track=socurites,football";
+}
